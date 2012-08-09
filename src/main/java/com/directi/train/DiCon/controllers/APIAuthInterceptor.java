20e20ccfad705ac968dac63a16ec39f9fca69ff4
niteesh3@gmail.com <niteesh3@gmail.com>
2012-08-09 17:10:10 +0530

    retweet + similar ppl

---
+++ src/main/java/com/directi/train/DiCon/controllers/APIAuthInterceptor.java
@@ -0,0 +1,11 @@
+package com.directi.train.DiCon.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: niteeshk
+ * Date: 7/8/12
+ * Time: 6:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class APIAuthInterceptor {
+}
---
+++ src/main/java/com/directi/train/DiCon/services/XSSHandler.java
@@ -0,0 +1,11 @@
+package com.directi.train.DiCon.services;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: niteeshk
+ * Date: 8/8/12
+ * Time: 1:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class XSSHandler {
+}
